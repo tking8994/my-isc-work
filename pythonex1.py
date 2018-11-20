5434e6c8a1ef17e2840e8432d3f4f268c50e7ca4
tking8994 <user01@see-l12070.leeds.ac.uk>
2018-11-20 15:10:45 +0000

    added pyex1

---
+++ pythonex1.py
@@ -0,0 +1,24 @@
+#exercise 1 part 1
+course="python"
+rating="9"
+print(course,rating)
+
+#exercise 1 part 2
+import math
+b=3
+c=4
+a=math.sqrt(b**2+c**2)
+print(a)
+
+#exercise 1 part 3
+
+type(a)
+type(b)
+type(c)
+
+#exercise 1 part 4
+
+a=int(a)
+
+print(str(a) + " squared eguals" + str(b) + " squared +" + str(c) + " squared.")
+
