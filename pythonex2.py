402ffc9ba97e4e032fa7bd63306eab94c022e223
tking8994 <user01@see-l12070.leeds.ac.uk>
2018-11-20 16:16:37 +0000

    added python2ex

---
+++ pythonex2.py
@@ -0,0 +1,27 @@
+while 1:
+    x=1
+
+#ex2 part 2
+gases=['He','Ne','Ar','Kr']
+print(gases)
+count=0
+while count<4:
+    item=gases[count]
+    print(item,count)
+    count+=1
+
+
+#ex2 part 3
+name=input("What is your name?")
+if name=="Lisa":
+    print(name + "plays saxophone")
+elif name=="Bart":
+    print(name + "rides skateboard")
+else:
+    print(name + "lives in springfield")
+
+#ex2 part 4
+
+
+1
+
